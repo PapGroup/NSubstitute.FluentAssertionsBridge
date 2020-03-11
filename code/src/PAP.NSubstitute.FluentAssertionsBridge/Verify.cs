eff984e7c00ca86b979514cb252ef75b40f6e9d8
Hadi Ahmadi <ahmadi.hadi@outlook.com>
2020-03-11 16:19:55 +0330

    summary for Verify class

--- code/src/PAP.NSubstitute.FluentAssertionsBridge/Verify.cs
+++ code/src/PAP.NSubstitute.FluentAssertionsBridge/Verify.cs
@@ -3,8 +3,17 @@ using NSubstitute.Core.Arguments;
 
 namespace PAP.NSubstitute.FluentAssertionsBridge
 {
+    /// <summary>
+    /// The class that integrates NSubstitute with FluentAssertions
+    /// </summary>
     public static class Verify
     {
+        /// <summary>
+        /// The method that verifies the arguments of called methods using FluentAssertions
+        /// </summary>
+        /// <typeparam name="T">type of arguments to verify</typeparam>
+        /// <param name="action">delegate that contains assertion</param>
+        /// <returns>parameter of called method</returns>
         public static T That<T>(Action<T> action)
         {
             return ArgumentMatcher.Enqueue(new FluentAssertionsMatcherAdapter<T>(action));
